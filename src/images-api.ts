3d971679dfa0b434f2b934df7fe4c9ab625c8394
IgorMatvienko <38344025+gorisgor@users.noreply.github.com>
2024-08-08 08:31:18 +0000

    Update

--- src/images-api.ts
+++ src/images-api.ts
@@ -3,8 +3,23 @@ import axios from "axios";
 const API_KEY = "KRNJhs0g10MZsM62FfXYN_CmaZP9RR3ihLQR1jBWkeU";
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
-export const fetchImages = async (query, currentPage) => {
-  const response = await axios.get("/search/photos", {
+interface Image {
+  id: string;
+  description: string | null;
+  alt_description: string | null;
+  urls: {
+    small: string;
+    thumb: string;
+  };
+}
+
+interface Response {
+  results: Image[];
+  total_pages: number;
+}
+
+export const fetchImages = async (query: string, currentPage: number) => {
+  const response = await axios.get<Response>("/search/photos", {
     params: {
       client_id: API_KEY,
       query: query,
